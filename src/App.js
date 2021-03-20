4a24a9ddf635a5ad00b54062d0d0b26abd3a0cd5
study-hary-id <haryansyah.study.id@gmail.com>
2021-03-20 10:08:50 +0700

    Edit App.js file Add AddPage and EditPage components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import HomePage from './pages/Home';
 import './App.css';
+import HomePage from './pages/Home';
+import AddPage from './pages/Add';
+import EditPage from './pages/Edit';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <AddPage />
+      <EditPage />
     </div>
   );
 }
