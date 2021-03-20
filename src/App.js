215f47f425b07716bf209c7ddee7b2dd0da5eede
study-hary-id <haryansyah.study.id@gmail.com>
2021-03-20 11:33:13 +0700

    Added react-router-dom Create routers with BrowserRouter Create route matcher with Route, Switch

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import HomePage from './pages/Home';
 import AddPage from './pages/Add';
 import EditPage from './pages/Edit';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <AddPage />
-      <EditPage />
+      <Switch>
+        <Route path="/add">
+          <AddPage />
+        </Route>
+        <Route path="/edit">
+          <EditPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
