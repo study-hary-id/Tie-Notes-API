779abeb219ea14cc563d21b6d3f24f01b69a0e6d
study-hary-id <haryansyah.study.id@gmail.com>
2021-04-02 16:09:51 +0700

    Add Message.js UI Implement Message UI in AddNoteForm

--- src/components/AddNoteForm.js
+++ src/components/AddNoteForm.js
@@ -2,9 +2,20 @@ import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { Form, FormGroup, Label, Input, TextArea } from './ui/Forms';
 import Button from './ui/Button';
+import Message from './ui/Message';
+import getLocalStorageData from '../utils/getLocalStorageData';
 
 const AddNoteForm = () => {
   const [state, setState] = useState({ title: '', note: '' });
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const addNoteId = (notes) => {
+    const noteId = uuidv4();
+
+    notes.push({ ...state, id: noteId });
+
+    return notes;
+  };
 
   const handleTitleChange = (e) => {
     setState({ ...state, title: e.target.value });
@@ -15,12 +26,11 @@ const AddNoteForm = () => {
   };
 
   const handleSubmit = (e) => {
-    const noteId = uuidv4();
-    const existing = localStorage.getItem('notes');
-    const notes = existing ? JSON.parse(existing) : [];
+    const notes = getLocalStorageData('notes');
 
-    notes.push({ ...state, id: noteId });
-    localStorage.setItem('notes', JSON.stringify(notes));
+    localStorage.setItem('notes', JSON.stringify(addNoteId(notes)));
+
+    setIsSuccess(true);
 
     e.preventDefault();
   };
@@ -28,19 +38,22 @@ const AddNoteForm = () => {
   const { title, note } = state;
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <FormGroup>
-        <Label>Title :</Label>
-        <Input type="text" name="title" value={title} onChange={handleTitleChange} />
-      </FormGroup>
-      <FormGroup>
-        <Label>Note :</Label>
-        <TextArea name="note" rows="12" value={note} onChange={handleNoteChange} />
-      </FormGroup>
-      <FormGroup>
-        <Button type="submit">Add</Button>
-      </FormGroup>
-    </Form>
+    <>
+      {isSuccess && <Message text="Data successfully saved in localStorage" />}
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label>Title :</Label>
+          <Input type="text" name="title" value={title} onChange={handleTitleChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label>Note :</Label>
+          <TextArea name="note" rows="12" value={note} onChange={handleNoteChange} />
+        </FormGroup>
+        <FormGroup>
+          <Button type="submit">Add</Button>
+        </FormGroup>
+      </Form>
+    </>
   );
 };
 
